c8db8e1daa8c38fa76ad74c81ba489a315d4ce19
Bender666 <138517045+Gergiy1203@users.noreply.github.com>
2026-03-24 18:00:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import socket
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, world!"}
+
+@app.get("/ping")
+def ping():
+    return {"status": "ok"}
+
+
